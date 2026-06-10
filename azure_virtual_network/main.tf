60a9bbd36b1f20c17cb1b0445cdebf5ba22c8936
theShubhamOnly <shubham@queuebuster.co>
2026-06-10 16:46:30 +0530

    Added Vnet provider/variable/Nested Vnet using For-each

---
+++ azure_virtual_network/main.tf
@@ -0,0 +1,7 @@
+resouce "azurerm_virtual_network" "v-net" {
+    for_each = var.vnet_network
+    name = each.value.name
+    location = each.value.location
+    address_space = each.value.address_space
+    resource_group_name = each.value.resource_group_name
+}
